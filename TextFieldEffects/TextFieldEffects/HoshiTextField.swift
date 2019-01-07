3571db196c26e274a63dd0034ee576b63368e4fa
Gonzalo Erro <errogonzalog@gmail.com>
2019-01-07 15:46:50 -0300

    fix(textfield): updating labels and frames

--- TextFieldEffects/TextFieldEffects/HoshiTextField.swift
+++ TextFieldEffects/TextFieldEffects/HoshiTextField.swift
@@ -128,16 +128,41 @@ typealias VoidClosure = () -> Void
   }
 
   private func configureUI() {
+
     initPlaceholderFont()
     configurePlaceholderLabelFrame()
     configurePlaceholderFont()
     configureErrorLabel()
     updateBorder()
     updatePlaceholder()
+
+    addSublayers()
+    addLabels()
+
+    frame = CGRect(x: frame.origin.x,
+                   y: frame.origin.y,
+                   width: frame.size.width ,
+                   height: height)
+  }
+
+  private func addSublayers() {
     layer.addSublayer(inactiveBorderLayer)
     layer.addSublayer(activeBorderLayer)
+  }
+
+  private func addLabels() {
     addSubview(placeholderLabel)
     addSubview(errorLabel)
+  }
+
+  open override func setNeedsLayout() {
+    super.setNeedsLayout()
+    updatePlaceholder()
+    updateBorder()
+    frame = CGRect(x: frame.origin.x,
+                   y: frame.origin.y,
+                   width: frame.size.width ,
+                   height: height)
   }
 
   open func showError(message: String) {
@@ -160,11 +185,7 @@ typealias VoidClosure = () -> Void
   // MARK: - TextFieldEffects
 
   override open func drawViewsForRect(_ rect: CGRect) {
-    frame = CGRect(x: frame.origin.x,
-                   y: frame.origin.y,
-                   width: rect.size.width ,
-                   height: height)
-    updateBorder()
+
   }
 
   open func setViewsForTextEntry() {
