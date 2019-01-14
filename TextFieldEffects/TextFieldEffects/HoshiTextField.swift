85d3ca150f61ecaf9bd08dee2dc286d8dfbcfd3f
Gonzalo Erro <errogonzalog@gmail.com>
2019-01-14 16:40:15 -0300

    fix(hoshitextfield): showing placeholder while showing error

--- TextFieldEffects/TextFieldEffects/HoshiTextField.swift
+++ TextFieldEffects/TextFieldEffects/HoshiTextField.swift
@@ -167,7 +167,7 @@ typealias VoidClosure = () -> Void
     errorLabel.textColor = borderActiveColor
     errorLabel.sizeToFit()
     errorLabel.isHidden = false
-    placeholderLabel.isHidden = true
+    placeholderLabel.isHidden = false
 
     activeBorderLayer.frame = rectForBorder(borderThicknessActive)
     activeBorderLayer.isHidden = false
